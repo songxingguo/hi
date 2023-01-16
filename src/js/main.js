0c1a72d862a873cc59cf1a440196c4bf1d9b0762
songxingguo <xg.song@qq.com>
2023-01-16 16:13:17 +0800

    feat: 调整旋转速度

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@ const { scene, camera, renderer } = threeFactory.init();
 function render() {
   renderer.render(scene, camera);
   const mesh = scene.children[0]
-  mesh.rotation.x += 0.005;
-  mesh.rotation.y += 0.01;
+  mesh.rotation.x += 0.0005;
+  mesh.rotation.y += 0.001;
   // 渲染下一帧的时候调用render函数
   requestAnimationFrame(render);
 }
