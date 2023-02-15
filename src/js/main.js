d948291a69897140249f06be8344a2ca59b49394
songxingguo <xg.song@qq.com>
2023-02-15 12:07:41 +0800

    feat: hover暂停，离开执行动画

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,31 @@
 // import * from './js/scroll.js';
-import './valine.js'
+import "./valine.js";
 // import './js/count.js'
-import './seo.js'
-import './../css/index.css';
+import "./seo.js";
+import "./../css/index.css";
 import ThreeFactory from "./ThreeFactory";
+
+const container = document.getElementById("three-container");
+container.addEventListener("mousemove", moveMouseToCanvas, false);
+container.addEventListener("mouseleave", leaveToCanvas, false);
+let voluntarily = true; // 开启动画
+function moveMouseToCanvas() {
+  console.log("moveMouseToCanvas", moveMouseToCanvas);
+  voluntarily = false;
+}
+function leaveToCanvas() {
+  voluntarily = true;
+}
 
 const threeFactory = new ThreeFactory();
 const { scene, camera, renderer } = threeFactory.init();
 function render() {
   renderer.render(scene, camera);
-  const mesh = scene.children[0]
-  mesh.rotation.x += 0.0005;
-  mesh.rotation.y += 0.001;
+  const mesh = scene.children[0];
+  if (voluntarily) {
+    mesh.rotation.x += 0.0005;
+    mesh.rotation.y += 0.001;
+  }
   // 渲染下一帧的时候调用render函数
   requestAnimationFrame(render);
 }
