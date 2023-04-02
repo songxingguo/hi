809d792db9ec563e642056a115e02590cf3cd1fd
songxingguo <1328989942@qq.com>
2023-04-02 21:39:50 +0800

    feat: 评论文案修改

--- src/js/valine.js
+++ src/js/valine.js
@@ -5,7 +5,7 @@ new Valine({
   appKey: '2oU4tvtGWAXp5i6kWAnhd2rm',
   notify: true, // 邮件提醒!!!
   verify: true, // 验证码
-  placeholder: 'ヾﾉ≧∀≦)o欢迎留下您的地址，让我有机会向您学习!',
+  placeholder: 'ヾﾉ≧∀≦)o欢迎留下您的主页地址，让我有机会向您学习!',
   avatar: 'wavatar',
   visitor: true, // 阅读量统计
 });
